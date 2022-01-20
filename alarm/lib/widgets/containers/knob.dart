b08637ef0b077007a872f04fe5fc53d4738aea57
Surafel Melese <surafelmelese09@gmail.com>
2022-01-21 01:34:52 +0300

    we have a moving alarm icon

--- alarm/lib/widgets/containers/knob.dart
+++ alarm/lib/widgets/containers/knob.dart
@@ -2,32 +2,112 @@ import 'package:alarm/widgets/painters/knob.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
 
-class Knob extends StatelessWidget {
-  double endAngle;
+class Knob extends StatefulWidget {
   final double radius;
   final double innerRadiusRatio;
   final double outerRadiusRatio;
-  Knob(
+  const Knob(
       {Key? key,
       required this.radius,
       required this.innerRadiusRatio,
-      required this.outerRadiusRatio,
-      required this.endAngle})
+      required this.outerRadiusRatio})
       : super(key: key);
 
   @override
+  State<Knob> createState() => _KnobState();
+}
+
+class _KnobState extends State<Knob> {
+  late double endAngle;
+  late Offset endLocalOffset;
+
+  @override
+  void initState() {
+    super.initState();
+    endAngle = pi;
+    endLocalOffset = Offset(
+        widget.radius * widget.outerRadiusRatio * 0.5,
+        widget.radius *
+            (widget.innerRadiusRatio +
+                (widget.outerRadiusRatio - widget.innerRadiusRatio) * 0.5));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-        height: radius,
-        width: radius,
-        //color: Colors.red,
-        child: Padding(
-          padding: EdgeInsets.all(radius * (1 - outerRadiusRatio) / 2),
-          child: CustomPaint(
-            size: Size.square(radius * outerRadiusRatio),
-            painter: KnobPainter(
-                0, endAngle, radius * (outerRadiusRatio - innerRadiusRatio)),
-          ),
-        ));
+    return Stack(
+      children: [
+        Container(
+            height: widget.radius,
+            width: widget.radius,
+            child: Padding(
+              padding: EdgeInsets.all(
+                  widget.radius * (1 - widget.outerRadiusRatio) / 2),
+              child: CustomPaint(
+                size: Size.square(widget.radius * widget.outerRadiusRatio),
+                painter: KnobPainter(
+                    0,
+                    endAngle,
+                    widget.radius *
+                        (widget.outerRadiusRatio - widget.innerRadiusRatio)),
+              ),
+            )),
+        Positioned(
+            top: endLocalOffset.dy,
+            left: endLocalOffset.dx,
+            child: GestureDetector(
+              onPanUpdate: (details) {
+                Offset currentLocation = Offset(
+                    endLocalOffset.dx + details.delta.dx,
+                    endLocalOffset.dy + details.delta.dy);
+                double x = currentLocation.dx -
+                    widget.radius * widget.outerRadiusRatio * 0.5;
+                double y = widget.radius * widget.outerRadiusRatio * 0.5 -
+                    currentLocation.dy;
+                if (x >= 0) {
+                  setState(() {
+                    endAngle = pi / 2 - atan(y / x);
+                    // endLocalOffset = getVector(x, y);
+                    endLocalOffset = getLocalPosition(currentLocation);
+                  });
+                } else {
+                  setState(() {
+                    endAngle = pi / 2 - atan(y / x) + pi;
+                    // endLocalOffset = getVector(x, y);
+                    endLocalOffset = getLocalPosition(currentLocation);
+                  });
+                }
+                print(details.globalPosition);
+              },
+              child: Container(
+                width: widget.radius *
+                    (widget.outerRadiusRatio - widget.innerRadiusRatio),
+                height: widget.radius *
+                    (widget.outerRadiusRatio - widget.innerRadiusRatio),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                ),
+                child: const Icon(
+                  Icons.notifications_none,
+                  color: Color.fromRGBO(167, 153, 240, 1),
+                ),
+              ),
+            )),
+      ],
+    );
+  }
+
+  Offset getLocalPosition(Offset currentLocation) {
+    double relativeX =
+        currentLocation.dx - widget.radius * widget.outerRadiusRatio * 0.5;
+    double relativeY =
+        widget.radius * widget.outerRadiusRatio * 0.5 - currentLocation.dy;
+    double oldlength = sqrt(relativeX * relativeX + relativeY * relativeY);
+    Offset unitVector = Offset(relativeX / oldlength, relativeY / oldlength);
+    Offset newVector = Offset(
+        unitVector.dx * widget.radius * widget.innerRadiusRatio * 0.5,
+        unitVector.dy * widget.radius * widget.innerRadiusRatio * 0.5);
+    return Offset(newVector.dx + widget.radius * widget.outerRadiusRatio * 0.5,
+        widget.radius * widget.outerRadiusRatio * 0.5 - newVector.dy);
   }
 }
--- alarm/lib/widgets/containers/knob_background.dart
+++ alarm/lib/widgets/containers/knob_background.dart
@@ -12,13 +12,13 @@ class KnobBackground extends StatefulWidget {
 }
 
 class _KnobBackgroundState extends State<KnobBackground> {
-  double endAngle = 2 * pi;
+  double endAngle = pi;
   @override
   Widget build(BuildContext context) {
-    double clockRatio = 0.65;
+    double clockRatio = 0.625;
     double gapRatio = 0.05;
-    double innerGapRatio = 0.15;
-    double knobWidth = 0.075;
+    double innerGapRatio = 0.175;
+    double knobWidth = 0.1;
     double knobInnerRadiusRatio = clockRatio + innerGapRatio;
     double knobOuterRadiusRatio = knobInnerRadiusRatio + knobWidth;
     assert(knobInnerRadiusRatio < knobOuterRadiusRatio &&
@@ -28,28 +28,10 @@ class _KnobBackgroundState extends State<KnobBackground> {
       height: widget.radius,
       child: Stack(
         children: [
-          Positioned(
-              child: GestureDetector(
-            onPanUpdate: (details) {
-              double x = details.localPosition.dx - widget.radius * 0.45;
-              double y = widget.radius * 0.45 - details.localPosition.dy;
-              double angleInDegrees = (atan(y / x) / pi) * 180;
-              if (x >= 0) {
-                setState(() {
-                  endAngle = pi / 2 - atan(y / x);
-                });
-              } else {
-                setState(() {
-                  endAngle = pi / 2 - atan(y / x) + pi;
-                });
-              }
-            },
-            child: Knob(
-                endAngle: endAngle,
-                radius: widget.radius,
-                innerRadiusRatio: knobInnerRadiusRatio,
-                outerRadiusRatio: knobOuterRadiusRatio),
-          )),
+          Knob(
+              radius: widget.radius,
+              innerRadiusRatio: knobInnerRadiusRatio,
+              outerRadiusRatio: knobOuterRadiusRatio),
           Padding(
               padding: EdgeInsets.all(widget.radius * gapRatio),
               child: Center(
